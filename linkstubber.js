89803e333fbeb90947e8f19ac892e9239a54a3bc
Clay Stewart <claytonmstewart@gmail.com>
2013-11-23 16:12:12 -0600

    Styled the html show page

--- linkstubber.js
+++ linkstubber.js
@@ -1,13 +1,17 @@
-var findLinks = function() {
-    var outside_links = $("a.ls-outside-link");
-    outside_links.each(function(i) {
-        console.log($(this).text());
-        $(this).attr('target', "_blank");
-    });
+var findLinks;
+
+findLinks = function() {
+  var outside_links;
+  outside_links = $("a.ls-outside-link");
+  return outside_links.each(function(i) {
+    var this_link_href;
+    console.log($(this).text());
+    $(this).attr("target", "_blank");
+    this_link_href = $(this).attr("href");
+    return console.log('the href', this_link_href);
+  });
 };
 
-
 $(document).ready(function() {
-	findLinks();
+  return findLinks();
 });
-
--- linkstubber.js.coffee
+++ linkstubber.js.coffee
@@ -1,8 +1,12 @@
+# returns all the links as an array
+# adds target blank to all in the proecess
 findLinks = ->
   outside_links = $("a.ls-outside-link")
   outside_links.each (i) ->
     console.log $(this).text()
     $(this).attr "target", "_blank"
+    this_link_href = $(this).attr "href"
+    console.log 'the href', this_link_href
 
 
 $(document).ready ->
