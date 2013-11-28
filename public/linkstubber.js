2e8795e2c11d4a9af4595faf5d9d317be47ff236
Clay Stewart <claytonmstewart@gmail.com>
2013-11-27 20:07:43 -0600

    Add cross domain ajax logic to javascript

--- public/linkstubber.js
+++ public/linkstubber.js
@@ -1,4 +1,36 @@
+// var makeAjaxCallOnLinkURL = function (urltoget) {
+// $.ajax({
+//     type: 'GET',
+//     url: "http://www.ajax-cross-domain.com/cgi-bin/ACD/ACD.js?uri=("+urltoget+")",
+//     // data: formData,
+//     crossDomain: true,
+//     dataType: 'script',
+//     cache: false,
+//     timeout: 7000, // seven seconds
+//     success: function(data){
+//       console.log(this.url);
+//       console.log(ACD.status);
+//     },
+//     error: function(XMLHttpRequest, textStatus, errorThrown)  {
+//       console.log('There was an ' + errorThrown + ' error due to a ' + textStatus + ' condition.');
+//       console.log(XMLHttpRequest);
+//     },
+//     complete: function(XMLHttpRequest, status)  {
+//       // nothing
+//       console.log('call completed');
+//     }
+//   });
+// };
+// makeAjaxCallOnLinkURL('http://www.claystewart.me');
+// makeAjaxCallOnLinkURL('http://asdfasafsafdasdfasdfasfasfasdfasfdaf.com');
 
+
+var makeAjaxCallOnLinkURL = function (urltoget) {
+	$('body').append('<script type="text/javascript" src="http://www.ajax-cross-domain.com/cgi-bin/ACD/ACD.js?uri=('+urltoget+')"></script>');
+	$('body').append('<script type="text/javascript"> var ACD;console.log(ACD);setTimeout(function(){var siteStatus = ACD.status;if(siteStatus === "200 OK") {console.log("This site exists");} else {console.log("This thing AINT here!");};console.log(ACD);},5000);</script>');
+};
+
+makeAjaxCallOnLinkURL('http://www.google.com/');
 //========================================================
 
 var consolLogThingAboutLinks, findLinks, makeTargetBlank, runMethodsOnAllLinks;
