d223115279ab5404715d0d0bc7206182bd5337e1
Clay Stewart <claytonmstewart@gmail.com>
2013-11-29 20:16:18 -0600

    Add a couple functions that start to check for the url existance server side

--- link-stubber.rb
+++ link-stubber.rb
@@ -2,7 +2,54 @@ require 'rubygems'
 require 'bundler/setup'
 require 'sinatra'
 require 'sinatra/reloader'
+require "net/http"
 
 get '/' do
   erb :index
+end
+
+get '/url' do
+  # note the URL always seems to need the closing slash /
+  url_string = "http://www.github.com/clamstew/"
+  # this page http://stackoverflow.com/questions/5908017/check-if-url-exists-in-ruby
+  # explained a quick use of Net::HTTP for this purpose
+  def url_exist?(url_string)
+    url = URI.parse(url_string)
+    req = Net::HTTP.new(url.host, url.port)
+    req.read_timeout = 100
+    res = req.request_head(url.path)
+    ##{res.code}" if res.code != "404" # false if returns 404 - not found
+    if res.kind_of?(Net::HTTPRedirection)
+      url_exist?(res['location']) # Go after any redirect and make sure you can access the redirected URL
+    else
+      ! %W(4 5).include?(res.code[0]) # Not from 4xx or 5xx families
+    end
+  rescue Errno::ENOENT
+    false # false if can't find the server
+  rescue Errno::ETIMEDOUT
+    "request timed out not cool"
+  end
+  "#{url_exist?(url_string)}"
+end
+
+get '/url2' do
+  def working_url?(url, max_redirects=6)
+    response = nil
+    seen = Set.new
+    loop do
+      url = URI.parse(url)
+      break if seen.include? url.to_s
+      break if seen.size > max_redirects
+      seen.add(url.to_s)
+      response = Net::HTTP.new(url.host, url.port).request_head(url.path)
+      if response.kind_of?(Net::HTTPRedirection)
+        url = response['location']
+      else
+        break
+      end
+    end
+    response.kind_of?(Net::HTTPSuccess) && url.to_s
+  end
+  url = "https://github.com/clamstew/"
+  "#{working_url?(url)}"
 end
--- public/linkstubber.js
+++ public/linkstubber.js
@@ -25,12 +25,75 @@
 // makeAjaxCallOnLinkURL('http://asdfasafsafdasdfasdfasfasfasdfasfdaf.com');
 
 
-var makeAjaxCallOnLinkURL = function (urltoget) {
-	$('body').append('<script type="text/javascript" src="http://www.ajax-cross-domain.com/cgi-bin/ACD/ACD.js?uri=('+urltoget+')"></script>');
-	$('body').append('<script type="text/javascript"> var ACD;console.log(ACD);setTimeout(function(){var siteStatus = ACD.status;if(siteStatus === "200 OK") {console.log("This site exists");} else {console.log("This thing AINT here!");};console.log(ACD);},5000);</script>');
-};
+// var makeAjaxCallOnLinkURL = function (urltoget) {
+//$('body').append('<script type="text/javascript" src="http://www.ajax-cross-domain.com/cgi-bin/ACD/ACD.js?uri=('+urltoget+')"></script>');
+//$('body').append('<script type="text/javascript"> var ACD;console.log(ACD);setTimeout(function(){var siteStatus = ACD.status;if(siteStatus === "200 OK") {console.log("This site exists");} else {console.log("This thing AINT here!");};console.log(ACD);},5000);</script>');
+// };
 
-makeAjaxCallOnLinkURL('http://www.google.com/');
+// makeAjaxCallOnLinkURL('http://www.google.com/');
+
+// function checkUrl(url){
+//     var request = new XMLHttpRequest();
+//     request.open('GET', url, true);
+//     request.send();
+//     request.onreadystatechange = function(){
+//         if(request.status==200){
+//             console.log(request.readyState);
+//             return true;
+//         }else{
+//             return false;
+//         }
+//     };
+// }
+
+// //Then I use the function to check and append the data to the page
+// var url = "http://www.apple.com";
+// if(checkUrl(url)){
+//     //work with the data
+//     console.log('work with the data');
+// }else{
+//     //do nothing
+//     console.log("do nothing");
+// }
+
+
+
+function isURLReal(fullyQualifiedURL) {
+    var URL = encodeURIComponent(fullyQualifiedURL),
+        dfd = $.Deferred(),
+        checkURLPromise = $.getJSON('http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22' + URL + '%22&format=json&diagnostics=true&callback=handleResponse');
+
+    console.log(URL);
+
+    checkURLPromise.done(function(response) {
+        // results should be null if the page 404s or the domain doesn't work
+        if (response.query.results) {
+            dfd.resolve(true);
+        } else {
+            dfd.reject(false);
+        }
+    })
+    .fail(function() {
+        dfd.reject('failed');
+    });
+
+    return dfd.promise();
+}
+
+// usage
+isURLReal('google.com')
+        .done(function(result) {
+            // yes
+            console.log('thing exists!');
+        })
+        .fail(function(result) {
+            // no, or request failed
+            console.log('thing DONT exist!');
+        });
+
+
+
+
 //========================================================
 
 var consolLogThingAboutLinks, findLinks, makeTargetBlank, runMethodsOnAllLinks;
